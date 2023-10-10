533e2b6c0b82a45abaf528709f90892cf3190999
Gabriel Lacerda <gabriellacerda@MacBook-Air-de-Gabriel.local>
2023-10-10 08:49:14 -0300

    creating testes using Selenium

--- selenium/e2e/products.js
+++ selenium/e2e/products.js
@@ -56,3 +56,75 @@ describe('products', () => {
 
   // Implement the remaining test cases in a similar manner
 });
+
+
+/// NEW CASES - Gabriel lacerda
+it('product name is correctly displayed', async () => {
+  // Navigate, find and assert product name
+  await driver.findElement(By.css('a[href="/admin/products/"]')).click();
+  const productName = await driver.findElement(By.css('.product-name-class')).getText();
+  assert.strictEqual(productName, 'Expected Product Name');
+});
+
+it('product image is displayed', async () => {
+  await driver.findElement(By.css('a[href="/admin/products/"]')).click();
+  const productImage = await driver.findElement(By.css('.product-image-class'));
+  assert.ok(productImage);
+});
+
+it('editing a product variant', async () => {
+  // Navigate, edit variant, save and then assert the change
+  await driver.findElement(By.css('a[href="/admin/products/"]')).click();
+  await driver.findElement(By.css('.edit-variant-button')).click();
+  await driver.findElement(By.id('variant-detail-input')).clear();
+  await driver.findElement(By.id('variant-detail-input')).sendKeys('New Variant Detail');
+  await driver.findElement(By.css('.save-variant-button')).click();
+  const variantDetail = await driver.findElement(By.id('variant-detail-text')).getText();
+  assert.strictEqual(variantDetail, 'New Variant Detail');
+});
+
+it('deleting a product variant', async () => {
+  // Navigate, delete a variant and assert it no longer exists
+  await driver.findElement(By.css('a[href="/admin/products/"]')).click();
+  await driver.findElement(By.css('.delete-variant-button')).click();
+  const variantExist = await driver.findElements(By.css('.specific-variant-class'));
+  assert.strictEqual(variantExist.length, 0);
+});
+
+it('add a new product', async () => {
+  await driver.findElement(By.css('.add-product-button')).click();
+  // Add the product details here
+  await driver.findElement(By.css('.save-product-button')).click();
+  const productName = await driver.findElement(By.css('.newly-added-product-name')).getText();
+  assert.strictEqual(productName, 'New Product Name');
+});
+
+it('check pagination functionality', async () => {
+  await driver.findElement(By.css('a[href="/admin/products/"]')).click();
+  const paginationButton = await driver.findElement(By.css('.pagination-button'));
+  assert.ok(paginationButton);
+  await paginationButton.click();
+  const secondPageProduct = await driver.findElement(By.css('.second-page-product'));
+  assert.ok(secondPageProduct);
+});
+
+it('filter products by name', async () => {
+  await driver.findElement(By.css('a[href="/admin/products/"]')).click();
+  await driver.findElement(By.css('.product-filter-input')).sendKeys('Filtered Product Name');
+  await driver.findElement(By.css('.filter-button')).click();
+  const filteredProductName = await driver.findElement(By.css('.filtered-product-name')).getText();
+  assert.strictEqual(filteredProductName, 'Filtered Product Name');
+});
+
+it('check product sorting', async () => {
+  await driver.findElement(By.css('a[href="/admin/products/"]')).click();
+  await driver.findElement(By.css('.sort-button')).click();
+  // Add assertions to check if products are sorted as expected
+});
+
+it('validate product details access', async () => {
+  await driver.findElement(By.css('a[href="/admin/products/"]')).click();
+  await driver.findElement(By.css('.product-detail-button')).click();
+  const productDetail = await driver.findElement(By.css('.product-detail-class')).getText();
+  assert.ok(productDetail);
+});
